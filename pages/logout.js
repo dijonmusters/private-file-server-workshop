a0d91b8976625f11f2cc66e13a808571839ad516
Jon Meyers <jonathon.d.meyers@gmail.com>
2021-10-13 19:28:52 +1100

    add redirect after logout

--- pages/logout.js
+++ pages/logout.js
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 import supabase from "../utils/supabase";
+import { useRouter } from "next/router";
 
 const Logout = () => {
+  const router = useRouter();
+
   useEffect(() => {
     const logout = async () => {
       await supabase.auth.signOut();
+      router.push("/");
     };
     logout();
   }, []);
